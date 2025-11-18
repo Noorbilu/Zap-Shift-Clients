604720e0b58b52f26da4ff54ced95f3da648ee3e
Noorbilu <nuralabonno@gmail.com>
2025-11-18 18:27:27 +0600

    Added new folders

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import HowItWorks from '../HowItWorks/HowItWorks';
 import OurService from '../OurService/OurService';
+import Brands from '../Brands/Brands';
+import Divider from '../Divider/Divider';
 
 const Home = () => {
     return (
@@ -9,6 +11,8 @@ const Home = () => {
             <Banner></Banner>
             <HowItWorks></HowItWorks>
             <OurService></OurService>
+            <Brands></Brands>
+            <Divider></Divider>
         </div>
     );
 };
--- src/Pages/Home/HowItWorks/HowItWorks.jsx
+++ src/Pages/Home/HowItWorks/HowItWorks.jsx
@@ -3,7 +3,7 @@ import icon from '../../../assets/bookingIcon.png'
 
 const HowItWorks = () => {
     return (
-        <div className='m-14'>
+        <div className='m-14 '>
             <h2 className='text-secondary font-bold text-2xl'>How It Works</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
                 <div className='m-5 card bg-base-200 shadow-xl p-5 gap-1.5'>
